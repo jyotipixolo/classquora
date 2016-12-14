ffb4f79c135bd09f977fb3ab4fa317809409f2fd
Abhay Amin <pixoloproductions@gmail.com>
2016-12-15 03:05:29 +0530

    question page design full

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,75 +2,134 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 
-    // With the new view caching in Ionic, Controllers are only called
-    // when they are recreated or on app start, instead of every page change.
-    // To listen for when this page is active (for example, to refresh data),
-    // listen for the $ionicView.enter event:
-    //$scope.$on('$ionicView.enter', function(e) {
-    //});
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
 
-    // Form data for the login modal
-    $scope.loginData = {};
+        // Form data for the login modal
+        $scope.loginData = {};
 
-    // Create the login modal that we will use later
-    $ionicModal.fromTemplateUrl('templates/login.html', {
-        scope: $scope
-    }).then(function (modal) {
-        $scope.modal = modal;
-    });
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
 
-    // Triggered in the login modal to close it
-    $scope.closeLogin = function () {
-        $scope.modal.hide();
-    };
+        // Triggered in the login modal to close it
+        $scope.closeLogin = function () {
+            $scope.modal.hide();
+        };
 
-    // Open the login modal
-    $scope.login = function () {
-        $scope.modal.show();
-    };
+        // Open the login modal
+        $scope.login = function () {
+            $scope.modal.show();
+        };
 
-    // Perform the login action when the user submits the login form
-    $scope.doLogin = function () {
-        console.log('Doing login', $scope.loginData);
+        // Perform the login action when the user submits the login form
+        $scope.doLogin = function () {
+            console.log('Doing login', $scope.loginData);
 
-        // Simulate a login delay. Remove this and replace with your login
-        // code if using a login system
-        $timeout(function () {
-            $scope.closeLogin();
-        }, 1000);
-    };
-})
-.controller('loginCtrl', function ($scope, $stateParams) {
+            // Simulate a login delay. Remove this and replace with your login
+            // code if using a login system
+            $timeout(function () {
+                $scope.closeLogin();
+            }, 1000);
+        };
+    })
+    .controller('loginCtrl', function ($scope, $stateParams) {
 
     })
-.controller('PlaylistsCtrl', function ($scope) {
-    $scope.playlists = [
-        {
-            title: 'Reggae',
-            id: 1
+    .controller('PlaylistsCtrl', function ($scope, $interval) {
+
+        $scope.usertypes = [
+            {
+                id: 1,
+                'type': 'teacher',
+                'image': 'img/teacher.png'
         },
-        {
-            title: 'Chill',
-            id: 2
+            {
+                id: 2,
+                'type': 'inhouse',
+                'image': 'img/inhouse.png'
         },
-        {
-            title: 'Dubstep',
-            id: 3
+            {
+                id: 3,
+                'type': 'non inhouse',
+                'image': 'img/noninhouse.png'
+        }
+    ];
+        $scope.questions = [
+            {
+                'id': 1,
+                'userid': 1,
+                'usertype': 1,
+                'username': 'Abhay Amin',
+                'time': '',
+                'subject': 'Physics',
+                'bookmarked': true,
+                'question': "What does the fox say ? What does the fox say ? What does the ofx say ?",
+                'answers': 2,
+                'verified': true,
+                'image': 'ionic.png'
+
         },
-        {
-            title: 'Indie',
-            id: 4
+            {
+                'id': 2,
+                'userid': 2,
+                'usertype': 2,
+                'username': 'Abhay Amin',
+                'time': '',
+                'subject': 'Biology',
+                'bookmarked': true,
+                'question': "What does the fox say ? What does the fox say ? What does the ofx say ?",
+                'answers': 4,
+                'verified': true,
+                'image': 'ionic.png'
+
         },
-        {
-            title: 'Rap',
-            id: 5
+            {
+                id: 3,
+                'userid': 3,
+                'usertype': 3,
+                'username': 'Abhay Amin',
+                'time': '',
+                'subject': 'Chemistry',
+                'bookmarked': true,
+                'question': "What does the fox say ? What does the fox say ? What does the ofx say ?",
+                'answers': 0,
+                'verified': false,
+                'image': ''
+
         },
-        {
-            title: 'Cowbell',
-            id: 6
+            {
+                id: 4,
+                'userid': 2,
+                'usertype': 2,
+                'username': 'Abhay Amin',
+                'time': '',
+                'subject': 'Maths',
+                'bookmarked': true,
+                'question': "What does the fox say ? What does the fox say ? What does the ofx say ?",
+                'answers': 4,
+                'verified': false,
+                'image': ''
+
         }
   ];
-})
+
+
+        $scope.checkoverlay = function () {
+            $interval(function() {
+                console.log($('#floatbtn').hasClass('active'));
+                $scope.floatactive = $('#floatbtn').hasClass('active');
+            }, 200, 1);
+
+        };
+    })
 
 .controller('PlaylistCtrl', function ($scope, $stateParams) {
 
