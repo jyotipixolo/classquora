8f768ed9a636d0ef29d81b6e8a71e97495ffff01
Abhay Amin <pixoloproductions@gmail.com>
2016-12-17 03:46:04 +0530

    register

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,7 +41,12 @@ angular.module('starter.controllers', [])
         };
     })
     .controller('loginCtrl', function ($scope, $stateParams) {
-    
+
+        /*ROUTING*/
+        $scope.gotoregistertab = function () {
+            $('select').material_select();
+        };
+
     })
     .controller('PlaylistsCtrl', function ($scope, $interval) {
 
@@ -141,15 +146,15 @@ angular.module('starter.controllers', [])
 
     })
     .controller('profileCtrl', function ($scope, $stateParams, $location) {
-    
-    /*ROUTING*/
-    $scope.gotoquestionsasked = function(){
-        $location.path("/app/playlists");
-    };
-    
-    
-    
-})
+
+        /*ROUTING*/
+        $scope.gotoquestionsasked = function () {
+            $location.path("/app/playlists");
+        };
+
+
+
+    })
     .controller('paqCtrl', function ($scope, $stateParams) {})
     .controller('bookmarksCtrl', function ($scope, $stateParams) {})
     .controller('askCtrl', function ($scope, $stateParams) {})
